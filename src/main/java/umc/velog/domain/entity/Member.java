49150b307e4af545e6004889ed79d87b1eb73ff8
Simzard10 <jysim0326@ajou.ac.kr>
2023-11-08 21:45:15 +0900

    디렉터리 구조 추가

---
+++ src/main/java/umc/velog/domain/entity/Member.java
@@ -0,0 +1,37 @@
+package umc.velog.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "USERNAME", nullable = false)
+    private String username;
+
+    @OneToMany(mappedBy = "writer")
+    List<Board> boards = new ArrayList<Board>();
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
