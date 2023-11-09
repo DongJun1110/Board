68bcc3f4eda626949574a866378afd8673468720
Simzard10 <jysim0326@ajou.ac.kr>
2023-11-09 14:35:27 +0900

    BoardDto 추가 - toDto 구현

--- src/main/java/umc/velog/dto/BoardDto.java
+++ src/main/java/umc/velog/dto/BoardDto.java
@@ -1,4 +1,38 @@
 package umc.velog.dto;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+import umc.velog.domain.entity.Board;
+import umc.velog.domain.entity.Member;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardDto {
+    private Long id;
+    private String title;
+    private String content;
+    private Member writer;
+    private LocalDate createdDate;
+    private LocalDate modifiedDate;
+    private int likeCount;
+    private int viewCount;
+
+    public static BoardDto toDto(Board boardEntity) {
+        return BoardDto.builder()
+                .id(boardEntity.getId())
+                .writer(boardEntity.getWriter())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .createdDate(boardEntity.getCreatedDate())
+                .modifiedDate(boardEntity.getModifiedDate())
+                .likeCount(boardEntity.getLikeCount())
+                .viewCount(boardEntity.getViewCount())
+                .build();
+    }
+
 }
