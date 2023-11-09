babf1a656013adf7af968aa4b0b212e37d1c0e23
Simzard10 <jysim0326@ajou.ac.kr>
2023-11-09 15:15:57 +0900

    BoardService 필요한 서비스 형태 추가 - 게시글 전체 목록 - 게시글 상세 조회 - 게시글 저장 - 게시글 좋아요

--- src/main/java/umc/velog/service/BoardService.java
+++ src/main/java/umc/velog/service/BoardService.java
@@ -1,13 +1,40 @@
 package umc.velog.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.velog.domain.entity.Board;
 import umc.velog.dto.BoardDto;
+import umc.velog.repository.BoardRepository;
 
 import java.util.List;
 
+@Service
 public class BoardService {
+    private BoardRepository boardRepository;
+    @Transactional
     public List<BoardDto> getBoardList() {
+
     }
 
+    @Transactional
     public BoardDto getPost(Long postId) {
+
     }
+
+    @Transactional
+    public BoardDto savePost(BoardDto boardDto) {
+        // Dto to entity
+        Board entity = Board.toEntity(boardDto);
+
+        // save
+        Board saveEntity = boardRepository.save(entity);
+
+        // Entity To Dto
+        BoardDto dto = BoardDto.toDto(saveEntity);
+
+        return dto;
+
+    }
+
+    // 게시글 좋아요 서비스 추가 필요
 }
