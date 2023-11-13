49db2657040296ed9c539d1950b075d7ef738ff7
cckim <r2222911r@ajou.ac.kr>
2023-11-13 13:42:39 +0900

    fix: CommentService 수정

--- src/main/java/umc/velog/service/CommentService.java
+++ src/main/java/umc/velog/service/CommentService.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.velog.domain.entity.Board;
 import umc.velog.domain.entity.Comment;
+import umc.velog.domain.entity.Member;
 import umc.velog.dto.comment.CommentDto;
 import umc.velog.repository.BoardRepository;
 import umc.velog.repository.CommentRepository;
+import umc.velog.repository.MemberRepository;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -20,17 +23,19 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
-    public Comment addCommentToBoard(Long boardId, CommentDto commentDto) {
+    public Comment addCommentToBoard(Long boardId, Long memberId, CommentDto commentDto) {
         Board board = boardRepository.findById(boardId).orElse(null);
+        Member writer = memberRepository.findById(memberId).orElse(null);
 
         if (board != null) {
             Comment comment = new Comment();
             comment.setContent(commentDto.getContent());
             comment.setCreatedAt(new Date());
             comment.setBoard(board);
-            comment.setWriter(commentDto.getWriter());
+            comment.setWriter(writer);
 
             return commentRepository.save(comment);
         }
