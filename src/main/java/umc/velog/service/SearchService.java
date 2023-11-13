f8ce3515efd0afbcebe2c83132a189ab39557576
cckim <r2222911r@ajou.ac.kr>
2023-11-14 00:38:47 +0900

    feat: SearchService 추가

--- src/main/java/umc/velog/service/SearchService.java
+++ src/main/java/umc/velog/service/SearchService.java
@@ -1,2 +1,22 @@
-package umc.velog.service;public class SearchService {
+package umc.velog.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.velog.domain.entity.Board;
+import umc.velog.repository.BoardRepository;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SearchService {
+
+    private final BoardRepository boardRepository;
+    @Transactional
+    public List<Board> search(String keyword) {
+        return boardRepository.findByTitleContaining(keyword);
+    }
 }
