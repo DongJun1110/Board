ed087f049273946170bbc8b94db1bfd9e39d4681
cckim <r2222911r@ajou.ac.kr>
2023-11-15 00:14:52 +0900

    Fix: HeartController 수정

---
+++ src/main/java/umc/velog/controller/HeartController.java
@@ -0,0 +1,29 @@
+package umc.velog.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import umc.velog.dto.heart.HeartDto;
+import umc.velog.service.HeartService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/heart")
+public class HeartController {
+
+    private final HeartService heartService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody HeartDto insert(@RequestBody HeartDto heartDto) throws Exception {
+        return heartService.insert(heartDto);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody HeartDto delete(@RequestBody HeartDto heartDto) throws Exception {
+        return heartService.delete(heartDto);
+    }
+
+}
