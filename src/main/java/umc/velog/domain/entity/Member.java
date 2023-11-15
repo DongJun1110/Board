5694a29ea39da219df7e5b49339ad3600285dc82
Simzard10 <jysim0326@ajou.ac.kr>
2023-11-15 16:23:02 +0900

    refactor: Member, User entity 병합

--- src/main/java/umc/velog/domain/entity/Member.java
+++ src/main/java/umc/velog/domain/entity/Member.java
@@ -1,18 +1,24 @@
-    package umc.velog.domain.entity;
+package umc.velog.domain.entity;
 
-    import jakarta.persistence.*;
-    import lombok.AllArgsConstructor;
-    import lombok.Data;
-    import lombok.NoArgsConstructor;
-    import java.util.ArrayList;
-    import java.util.Date;
-    import java.util.List;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
 
-    @Entity
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public class Member {
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Member implements UserDetails {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +27,9 @@
 
         @Column(name = "USERNAME", nullable = false)
         private String username;
+
+        @Column
+        private String userId;
 
         @OneToMany(mappedBy = "writer", cascade = CascadeType.ALL)
         private List<Board> boards = new ArrayList<>();
@@ -33,5 +42,37 @@
 
         @Temporal(TemporalType.TIMESTAMP)
         private Date createdDate;
+
+        @Enumerated(EnumType.STRING)
+        private Role role;
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            System.out.println("entity " + authorities);
+            authorities.add(new SimpleGrantedAuthority(role.toString()));
+            System.out.println("entity " + authorities);
+            return authorities;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return false;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return false;
+        }
 
     }
