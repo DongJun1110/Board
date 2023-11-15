43eeb0420669ad7aa99013a19599612fe3dd8224
cckim <r2222911r@ajou.ac.kr>
2023-11-15 14:21:46 +0900

    Fix: Field추가에 따른 Method 요소 변경

--- src/main/java/umc/velog/dto/member/MemberDto.java
+++ src/main/java/umc/velog/dto/member/MemberDto.java
@@ -4,6 +4,7 @@ import lombok.*;
 import umc.velog.domain.entity.Board;
 import umc.velog.domain.entity.Comment;
 import umc.velog.domain.entity.Member;
+import umc.velog.domain.entity.Role;
 import umc.velog.dto.comment.CommentDto;
 
 import java.time.LocalDateTime;
@@ -20,19 +21,23 @@ public class MemberDto {
 
     private Long id;
     private String username;
+    private String userId;
     private List<Long> boardIds; // Assuming only board IDs are needed in the DTO
     private String password;
     private String email;
     private Date createdDate;
+    private Role role;
 
     public static MemberDto toDto(Member memberEntity) {
         return MemberDto.builder()
                 .id(memberEntity.getId())
                 .username(memberEntity.getUsername())
+                .userId(memberEntity.getUserId())
                 .boardIds(memberEntity.getBoards().stream().map(Board::getId).collect(Collectors.toList()))
                 .password(memberEntity.getPassword())
                 .email(memberEntity.getEmail())
                 .createdDate(memberEntity.getCreatedDate())
+                .role(memberEntity.getRole())
                 .build();
     }
 
