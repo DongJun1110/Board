ae342e84f024f353db1e25656f2bba7a107c8172
Simzard10 <jysim0326@ajou.ac.kr>
2023-11-15 16:26:27 +0900

    refactor: Optional 처리 추가

--- src/main/java/umc/velog/repository/MemberRepository.java
+++ src/main/java/umc/velog/repository/MemberRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import umc.velog.domain.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUserId(String userId);
+
     List<Member> findAllById(Long id);
 }
