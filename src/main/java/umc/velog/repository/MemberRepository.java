abfa9b636f571fa2c6389242cdbd32de1e200428
cckim <r2222911r@ajou.ac.kr>
2023-11-15 15:44:32 +0900

    Fix: MemberRepository 수정

--- src/main/java/umc/velog/repository/MemberRepository.java
+++ src/main/java/umc/velog/repository/MemberRepository.java
@@ -3,9 +3,12 @@ package umc.velog.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.velog.domain.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUserId(String userId);
+
+    List<Member> findAllById(Long id);
 
 }
