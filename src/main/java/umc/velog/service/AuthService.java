bb690f8b90caa4de6078938b204f10bcb8fa8d42
Simzard10 <jysim0326@ajou.ac.kr>
2023-11-15 16:27:57 +0900

    feat: 회원가입 부분 추가

--- src/main/java/umc/velog/service/AuthService.java
+++ src/main/java/umc/velog/service/AuthService.java
@@ -1,14 +1,13 @@
 package umc.velog.service;
 
+import umc.velog.domain.entity.Member;
+import umc.velog.dto.user.UserJoinDto;
 import umc.velog.security.TokenInfo;
 import umc.velog.dto.user.UserInfoDto;
-import umc.velog.dto.user.UserJoinDto;
 import umc.velog.dto.user.UserLoginDto;
 
 public interface AuthService {
     TokenInfo login(UserLoginDto userLoginDto);
-
-    void join(UserJoinDto userJoinDto);
-
+    Member join(UserJoinDto userJoinDto) throws Exception;
     UserInfoDto info();
 }
