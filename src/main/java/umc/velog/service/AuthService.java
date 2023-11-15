732978088b4a96dae3faaf0e03fb0ff3c606bb04
cckim <r2222911r@ajou.ac.kr>
2023-11-15 14:30:18 +0900

    Feat: AuthService 추가

--- src/main/java/umc/velog/service/AuthService.java
+++ src/main/java/umc/velog/service/AuthService.java
@@ -1,5 +1,6 @@
 package umc.velog.service;
 
+import umc.velog.domain.entity.Member;
 import umc.velog.security.TokenInfo;
 import umc.velog.dto.UserInfoDto;
 import umc.velog.dto.UserJoinDto;
@@ -7,8 +8,6 @@ import umc.velog.dto.UserLoginDto;
 
 public interface AuthService {
     TokenInfo login(UserLoginDto userLoginDto);
-
-    void join(UserJoinDto userJoinDto);
-
+    Member join(UserJoinDto userJoinDto) throws Exception;
     UserInfoDto info();
 }
