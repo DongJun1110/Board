cc82e5f2d0aeb60262f75efb248e695ed4ea1b9c
cckim <r2222911r@ajou.ac.kr>
2023-11-15 14:25:30 +0900

    Fix: MemberRepository 메소드 변경

--- src/main/java/umc/velog/repository/MemberRepository.java
+++ src/main/java/umc/velog/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package umc.velog.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.velog.domain.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUserId(String userId);
+
 }
