eca72cf32302d76cb9ed818e88d8bf4feed23354
cckim <r2222911r@ajou.ac.kr>
2023-11-15 15:49:40 +0900

    Fix: User 엔티티를 Member 엔티티에 병합

--- src/main/java/umc/velog/domain/entity/Member.java
+++ src/main/java/umc/velog/domain/entity/Member.java
@@ -5,12 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -23,59 +21,59 @@ import java.util.List;
 @Builder
 public class Member implements UserDetails {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "WRITER_ID", nullable = false)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "WRITER_ID", nullable = false)
+        private Long id;
 
-    @Column(name = "USERNAME", nullable = false)
-    private String username;
+        @Column(name = "USERNAME", nullable = false)
+        private String username;
 
-    @Column
-    private String userId;
+        @Column
+        private String userId;
 
-    @OneToMany(mappedBy = "writer", cascade = CascadeType.ALL)
-    private List<Board> boards = new ArrayList<>();
+        @OneToMany(mappedBy = "writer", cascade = CascadeType.ALL)
+        private List<Board> boards = new ArrayList<>();
 
-    @Column(nullable = false)
-    private String password;
+        @Column(nullable = false)
+        private String password;
 
-    @Column(nullable = false)
-    private String email;
+        @Column(nullable = false)
+        private String email;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
+        @Temporal(TemporalType.TIMESTAMP)
+        private Date createdDate;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
+        @Enumerated(EnumType.STRING)
+        private Role role;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> authorities = new ArrayList<>();
-        System.out.println("entity " + authorities);
-        authorities.add(new SimpleGrantedAuthority(role.toString()));
-        System.out.println("entity " + authorities);
-        return authorities;
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            System.out.println("entity " + authorities);
+            authorities.add(new SimpleGrantedAuthority(role.toString()));
+            System.out.println("entity " + authorities);
+            return authorities;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return false;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return false;
+        }
+
     }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
-}
