f8b45a6e25a31e7f4d53d166053f661d71cf4a35
cckim <r2222911r@ajou.ac.kr>
2023-11-15 14:31:26 +0900

    Fix: 필수 생성자 어노테이션 추가 및 User -> Member 변경

--- src/main/java/umc/velog/service/AuthServiceImpl.java
+++ src/main/java/umc/velog/service/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package umc.velog.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,63 +10,65 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import umc.velog.domain.entity.Member;
+import umc.velog.repository.MemberRepository;
 import umc.velog.security.JwtTokenProvider;
-import umc.velog.security.Role;
+import umc.velog.domain.entity.Role;
 import umc.velog.security.SecurityUtil;
 import umc.velog.security.TokenInfo;
-import umc.velog.domain.entity.User;
 import umc.velog.dto.UserInfoDto;
 import umc.velog.dto.UserJoinDto;
 import umc.velog.dto.UserLoginDto;
-import umc.velog.repository.UserRepository;
+
+import java.util.Date;
 
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService, UserDetailsService {
 
-    @Autowired
-    private final UserRepository userRepository;
-
-    @Autowired
+    private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
-
-    @Autowired
     private final JwtTokenProvider jwtTokenProvider;
-
-    public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
 
     @Override
     public TokenInfo login(UserLoginDto userLoginDto) {
-        User user = userRepository.findByUserId(userLoginDto.getUserId()).orElseThrow(() -> new UsernameNotFoundException("아이디 혹은 비밀번호를 확인하세요."));
+        Member member = memberRepository.findByUserId(userLoginDto.getUserId()).orElseThrow(() -> new UsernameNotFoundException("아이디 혹은 비밀번호를 확인하세요."));
 
-        boolean matches = passwordEncoder.matches(userLoginDto.getPassword(), user.getPassword());
+        boolean matches = passwordEncoder.matches(userLoginDto.getPassword(), member.getPassword());
         if (!matches) throw new BadCredentialsException("아이디 혹은 비밀번호를 확인하세요.");
 
-        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getUserId(), user.getPassword(), user.getAuthorities());
+        Authentication authentication = new UsernamePasswordAuthenticationToken(member.getUserId(), member.getPassword(), member.getAuthorities());
 
         TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
-        tokenInfo.setEmail(user.getUserId());
-        tokenInfo.setMemberRole(user.getRole().toString());
+        tokenInfo.setEmail(member.getUserId());
+        tokenInfo.setMemberRole(member.getRole().toString());
         return tokenInfo;
     }
 
     @Override
-    public void join(UserJoinDto userJoinDto) {
-        User user = new User();
-        user.setUserId(userJoinDto.getUserId());
-        user.setPassword(passwordEncoder.encode(userJoinDto.getPassword()));
+    public Member join(UserJoinDto userJoinDto) throws Exception {
+
+        String inputPassword = userJoinDto.getPassword();
+        String inputConfirmPassword = userJoinDto.getConfirmPassword();
+
+        if (!(isTwoPasswordAlign(inputPassword, inputConfirmPassword))) {
+            throw new Exception();
+        }
+
+        Member member = new Member();
+        member.setUserId(userJoinDto.getUserId());
+        member.setPassword(passwordEncoder.encode(userJoinDto.getPassword()));
         if(userJoinDto.getRole().equals("ROLE_ADMIN")) {
-            user.setRole(Role.ADMIN);
+            member.setRole(Role.ADMIN);
         }
         else {
-            user.setRole(Role.USER);
+            member.setRole(Role.USER);
         }
-        user.setUserName(userJoinDto.getUserName());
-        userRepository.save(user);
+        member.setUsername(userJoinDto.getUserName());
+        member.setEmail(userJoinDto.getEmail());
+        member.setCreatedDate(new Date());
+        return memberRepository.save(member);
     }
 
     @Override
@@ -76,16 +79,22 @@ public class AuthServiceImpl implements AuthService, UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-        return userRepository.findByUserId(userId)
+        return  memberRepository.findByUserId(userId)
                 .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
     }
 
-    private UserDetails createUserDetails(User user) {
+    private UserDetails createUserDetails(Member member) {
         return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getUsername())
-                .password(passwordEncoder.encode(user.getPassword()))
-                .roles(user.getRole().toString())
+                .username(member.getUsername())
+                .password(passwordEncoder.encode(member.getPassword()))
+                .roles(member.getRole().toString())
                 .build();
+    }
+
+    private boolean isTwoPasswordAlign(String password, String confirmPassword) {
+        if(password.equals(confirmPassword)){
+            return true;
+        }else return false;
     }
 }
