070c97b56ad2e9b58a5c9bf412fefce9b8144cec
cckim <r2222911r@ajou.ac.kr>
2023-11-15 14:22:35 +0900

    Fix: 확인 비밀번호 추가

--- src/main/java/umc/velog/dto/UserJoinDto.java
+++ src/main/java/umc/velog/dto/UserJoinDto.java
@@ -3,43 +3,18 @@ package umc.velog.dto;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import umc.velog.domain.entity.Role;
+
+import java.util.Date;
 
 @Data
 public class UserJoinDto {
+
     private String userId;
-    private String password;
-    private String role;
     private String userName;
+    private String password;
+    private String confirmPassword;
+    private String email;
+    private Role role;
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 }
