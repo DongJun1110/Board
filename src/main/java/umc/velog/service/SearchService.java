5f71b2bb502fb7e1466b1b3fc9085088ec4350a5
cckim <r2222911r@ajou.ac.kr>
2023-11-16 17:19:59 +0900

    Fix: SearchResponseDto 타입에 맞게 메서드 변경

--- src/main/java/umc/velog/service/SearchService.java
+++ src/main/java/umc/velog/service/SearchService.java
@@ -2,11 +2,14 @@ package umc.velog.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.velog.domain.entity.Board;
+import umc.velog.dto.search.SearchResponseDto;
 import umc.velog.repository.BoardRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,8 +18,21 @@ import java.util.List;
 public class SearchService {
 
     private final BoardRepository boardRepository;
+
+    List<SearchResponseDto> searchList = new ArrayList<>();
     @Transactional
-    public List<Board> search(String keyword) {
-        return boardRepository.findByTitleContaining(keyword);
+    public List<SearchResponseDto> search(String keyword) {
+        List<Board> byTitleContaining = boardRepository.findByTitleContaining(keyword);
+        for (Board board : byTitleContaining) {
+            SearchResponseDto result = new SearchResponseDto();
+            result.setPostImg(board.getPostImg());
+            result.setUserName(board.getWriter().getUsername());
+            result.setCommentsCount(board.getComments().size());
+            result.setLikeCount(board.getLikeCount());
+            result.setCreatedDate(board.getCreatedDate());
+
+            searchList.add(result);
+        }
+        return searchList;
     }
 }
