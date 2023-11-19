99036e806fbff0234206a8ae3566aefe78fa112c
cckim <r2222911r@ajou.ac.kr>
2023-11-19 14:21:18 +0900

    Feat: RequestCommentDto 추가

---
+++ src/main/java/umc/velog/dto/comment/RequestCommentDto.java
@@ -0,0 +1,14 @@
+package umc.velog.dto.comment;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+public class RequestCommentDto {
+
+    private String content;
+}
