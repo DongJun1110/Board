98ed84b1c5853b791c71ce532e0d3c8f7ba5aaa5
cckim <r2222911r@ajou.ac.kr>
2023-11-21 17:22:57 +0900

    Fix: 비회원 처리 및 하트 중복 처리

--- src/main/java/umc/velog/service/HeartService.java
+++ src/main/java/umc/velog/service/HeartService.java
@@ -1,15 +1,23 @@
 package umc.velog.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.velog.domain.entity.Board;
 import umc.velog.domain.entity.Heart;
 import umc.velog.domain.entity.Member;
 import umc.velog.dto.heart.HeartDto;
+import umc.velog.dto.user.UserInfoDto;
 import umc.velog.repository.BoardRepository;
 import umc.velog.repository.HeartRepository;
 import umc.velog.repository.MemberRepository;
+import umc.velog.security.SecurityUtil;
+
+import java.security.Security;
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,32 +30,52 @@ public class HeartService {
     @Transactional
     public HeartDto insert(HeartDto heartDto) throws Exception{
 
-        Member member = memberRepository.findById(heartDto.getMemberId())
-                .orElseThrow(() -> new Exception("Not found member id : " + heartDto.getMemberId()));
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
         Board board = boardRepository.findById(heartDto.getBoardId())
                 .orElseThrow(() -> new Exception("Not found board id : " + heartDto.getBoardId()));
 
-        if (heartRepository.findByMemberAndBoard(member, board).isPresent()) {
-            throw new Exception();
+        if (authentication != null && authentication.isAuthenticated()) {
+
+            String userId = SecurityUtil.getCurrentMemberId().getUserId();
+
+            Optional<Member> memberOptional = memberRepository.findByUserId(userId);
+            if (memberOptional.isPresent()) {
+
+                Member member = memberOptional.get();
+
+                if (heartRepository.findByMemberAndBoard(member, board).isPresent()) {
+                    return null;
+                }
+
+                Heart heart = Heart.builder()
+                        .board(board)
+                        .member(member)
+                        .build();
+
+                board.setLikeCount(board.getLikeCount() + 1);
+                heartRepository.save(heart);
+                boardRepository.save(board);
+
+                return heartDto;
+
+            } else {
+                System.out.println("userId에 해당하는 작성자를 찾을 수 없습니다: " + userId);
+            }
+        } else {
+            // 사용자가 인증되어 있지 않은 경우 처리
+            // 예외를 throw하거나 메시지를 기록하는 등의 방법으로 처리할 수 있습니다
+            System.out.println("사용자가 인증되어 있지 않습니다");
         }
-
-        Heart heart = Heart.builder()
-                .board(board)
-                .member(member)
-                .build();
-        heartRepository.save(heart);
-
-        board.setLikeCount(board.getLikeCount() + 1);
-        boardRepository.save(board);
-
-        return heartDto;
+        return null;
     }
 
     @Transactional
     public HeartDto delete(HeartDto heartDTO) throws Exception {
 
-        Member member = memberRepository.findById(heartDTO.getMemberId())
-                .orElseThrow(() -> new Exception("Could not found member id : " + heartDTO.getMemberId()));
+        String userId = SecurityUtil.getCurrentMemberId().getUserId();
+        Optional<Member> memberOptional = memberRepository.findByUserId(userId);
+        Member member = memberOptional.get();
 
         Board board = boardRepository.findById(heartDTO.getBoardId())
                 .orElseThrow(() -> new Exception("Could not found board id : " + heartDTO.getBoardId()));
