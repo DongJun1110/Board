1531f6f71b15a312a77e8ce8abd6115129244ae0
cckim <r2222911r@ajou.ac.kr>
2023-11-22 22:06:25 +0900

    Fix: 뷰 반환 삭제, 상태 코드 반환

--- src/main/java/umc/velog/controller/BoardController.java
+++ src/main/java/umc/velog/controller/BoardController.java
@@ -23,13 +23,13 @@ import java.util.Map;
 @Controller
 @RequestMapping("/boards")
 @RequiredArgsConstructor
+@RestController
 public class BoardController {
 
     private final BoardService boardService;
 
     // 전체 글 보기 페이지(홈)
     @GetMapping("")
-    @ResponseBody
     public List<BoardDto> list() {
         List<BoardDto> boardList = boardService.getBoardList();
         return boardList;
@@ -37,24 +37,16 @@ public class BoardController {
 
     // 상세 글 보기(게시글 페이지 이동)
     @GetMapping("/{boardId}")
-    @ResponseBody
     public BoardResponseDto detail(@PathVariable("boardId") Long boardId) {
         return boardService.getPost(boardId);
     }
 
-    @GetMapping("/write-form")
-    public String write() {
-        return "board/write-form";
-    }
-
     @PostMapping("/write-form")
-    @ResponseBody
     public ResponseEntity<Map<String, Object>> write(@RequestPart("data") BoardRequestDto boardRequestDto,
-                                                     @RequestPart(required = false) MultipartFile image,
-                                                     HttpServletResponse res) throws IOException {
+                                                     @RequestPart(required = false) MultipartFile image) throws IOException {
 
         if(!SecurityUtil.isLoginStatus()){
-            res.sendRedirect("/auth/loginPage");
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
         Long boardId = boardService.savePost(boardRequestDto, image);
@@ -64,11 +56,10 @@ public class BoardController {
         response.put("boardId", boardId);
         response.put("userId", userId);
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("/{userId}/profiles")
-    @ResponseBody
     public List<MemberDto> getBoardByUserId(@PathVariable String userId) {
         return boardService.getBoardByUserId(userId);
     }
